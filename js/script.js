5e7dd176d9ea0af0865d9cfa32a349512caf92d8
Brenda Butler <brendabutler@Horbut-Family-iMac.local>
2021-01-10 20:03:59 -0800

    include project links

--- js/script.js
+++ js/script.js
@@ -20,15 +20,20 @@ const projects = document.querySelectorAll('.project-about');
 let portfolioDiv = document.querySelector('.portfolio-cards');
 let portfolioContent = '';
 
-console.log(projects);
+const projectLinks = [
+  "https://brendarenee.github.io/techdegree-project-5/",
+  "https://brendarenee.github.io/techdegree-project-6/",
+  "https://brendarenee.github.io/techdegree-project-7/",
+  "https://brendarenee.github.io/techdegree-project-8/"
+]
 
 function loadProjects () {
-  projects.forEach( project => {
+  projects.forEach( (project, index) => {
     let id = project.getAttribute('id');
     let description = project.textContent;
     let title = id.split('-').join(' ').toUpperCase();
     portfolioContent +=
-      `<div class="col mb-4 bg-transparent px-0"><div class="card bg-transparent border-0 mx-auto" style="width:280px;"><div class="flip-card bg-transparent" style="width: 280px; height: 280px;"><div class="flip-card-inner position-relative w-100 h-100 text-center"><div class="flip-card-front w-100 h-100 d-flex flex-column justify-content-center position-absolute text-dark rounded-3" style="background:url('images/projects/${id}.png')"><h5 class="card-title px-3 display-6">${title}</h5></div><div class="flip-card-back rounded-3 w-100 h-100 d-flex flex-column justify-content-center position-absolute bg-light text-dark"><p class="card-text p-3">${description}</p></div></div></div></div></div>`;
+      `<a href="${projectLinks[index]}" target="_blank"><div class="col mb-4 bg-transparent px-0"><div class="card bg-transparent border-0 mx-auto" style="width:280px;"><div class="flip-card bg-transparent" style="width: 280px; height: 280px;"><div class="flip-card-inner position-relative w-100 h-100 text-center"><div class="flip-card-front w-100 h-100 d-flex flex-column justify-content-center position-absolute text-dark rounded-3" style="background:url('images/projects/${id}.png')"><h5 class="card-title px-3 display-6">${title}</h5></div><div class="flip-card-back rounded-3 w-100 h-100 d-flex flex-column justify-content-center position-absolute bg-light text-dark"><p class="card-text p-3">${description}</p></div></div></div></div></div></a>`;
   });
 };
 
